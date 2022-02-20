76320350675b086303b3c443cb867db7c4751ee2
Tetiana Nahiieva <nagieva@ukr.net>
2022-02-20 13:17:01 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    $('.our-works__slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1, 
+      variableWidth: true,
+      infinite: false,
+
+   });
+ });
+ 
+ 
